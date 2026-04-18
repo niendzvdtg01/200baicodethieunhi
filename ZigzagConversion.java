ac563b8298b0de5c2c878916e98dcc7c84429624
niendzvdtg01 <duynien34@gmail.com>
2026-04-18 23:59:44 +0700

    Zigzag convension

--- ZigzagConversion.java
+++ ZigzagConversion.java
@@ -1,15 +1,28 @@
+import java.util.*;
+
 public class ZigzagConversion {
     public static String convert(String s, int numRows) {
-        return s;
+        if (numRows == 1)
+            return s;
+
+        List<Character> res = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            int increment = 2 * (numRows - 1);
+            for (int j = i; j < s.length(); j += increment) {
+                res.add(s.charAt(j));
+                if (i > 0 && i < numRows - 1 && j + increment - 2 * i < s.length()) {
+                    res.add(s.charAt(j + increment - 2 * i));
+                }
+            }
+        }
+        String result = res.stream()
+                .map(String::valueOf)
+                .collect(java.util.stream.Collectors.joining());
+        return result;
     }
 
     public static void main(String[] args) {
-        int[][] nums = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(nums[j][i]);
-            }
-        }
-        // System.out.println();
+        String s = "PAYPALISHIRING";
+        System.out.println(convert(s, 3));
     }
 }
