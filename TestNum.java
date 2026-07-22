30059a31daca8ab5d4e7ecc704abb0d5d229f024
niendzvdtg01 <duynien34@gmail.com>
2026-07-22 23:01:38 +0700

    Add TestNum class and binary.py for floating-point precision demonstration

---
+++ TestNum.java
@@ -0,0 +1,11 @@
+public class TestNum {
+    public static void main(String[] agrs) {
+        double price = 59.9, quty = 10;
+        System.out.println(price * quty);
+        float sum = 0;
+        for (int i = 0; i < 10; i++)
+            sum += 0.1;
+        System.out.println(sum);
+        System.out.println(sum == 1);
+    }
+}
